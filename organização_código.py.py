e807421cf3ef17255fea4994a17eeecb2f4049b0
Robsonceia <114686103+Robsonceia@users.noreply.github.com>
2023-03-26 20:31:16 -0300

    Update organização_código.py.py
    Organização do código - parte 2

--- organização_código.py.py
+++ organização_código.py.py
@@ -7,14 +7,9 @@
 //Saida
 [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];'''
 
-elementos = [[0,1,2,3],
-             [11,12,13,4],
-             [10,15,14,5],
-             [9,8,7,6]]
+entrada1 = [0,1,2,3]
+entrada2 = [11,12,13,4]
+entrada3 = [10,15,14,5]
+entrada4 = [9,8,7,6]
 
-e1 = elementos[0]
-e2 = elementos[1]
-e3 = elementos[2]
-e4 = elementos[3]
-
-print(sorted(e1 + e2 + e3 + e4))
+elementos = []
