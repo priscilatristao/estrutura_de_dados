fe703f6d3f601685ec4f52c1c01acccffbdad789
priscilatristao <127314175+priscilatristao@users.noreply.github.com>
2023-03-26 20:56:14 -0300

    Update Organização_código.py.py
    Enunciado do projeto

--- Organização_código.py.py
+++ Organização_código.py.py
@@ -1,3 +1,9 @@
+#Tendo em vista o conjunto de elementos abaixo, criar um algoritimo que seja capaz de ordenar de forma crescente e decrescente, 
+#assim como imprimir de forma gráfica a notação Big’O do seu resultado //Entrada elementos = [[0,1,2,3],[11,12,13,4],[10,15,14,5],[9,8,7,6],]
+#//Saída [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+
+
+
 '''elementos = [[0,1,2,3],[11,12,13,4],[10,15,14,5],[9,8,7,6],]
 //Saida
 [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];'''
